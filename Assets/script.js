555af5a63f720f3331954ed13f837fad4b29910e
AKam <andy.kaminski42@gmail.com>
2022-08-25 19:54:09 -0400

    Saved league_id and User_id to localstorage

--- Assets/script.js
+++ Assets/script.js
@@ -8,8 +8,10 @@ function getLeagues () {
   .then(function(info) {
     console.log(info)
     var userID = info.user_id;
+    localStorage.setItem('user_id', JSON.stringify(userID));
     console.log(userID);
-    var season = '2021'; // possible input value?
+    var season = moment().format('YYYY'); 
+    console.log(season)
     var leaguesURL = "https://api.sleeper.app/v1/user/" + userID + "/leagues/nfl/" + season;
     console.log(leaguesURL)
     fetch(leaguesURL) 
@@ -20,9 +22,15 @@ function getLeagues () {
       console.log(info)
       console.log(info[0].name)
       for (var i=0; i<info.length; i++) {
-        var leagueEl = document.createElement("h3");
+        var leagueEl = document.createElement("button");
         leagueEl.textContent = info[i].name;
-        leagueEl.id = info[i].league_id;
+        var leagueID = info[i].league_id;
+        leagueEl.id = leagueID;
+        leagueEl.onclick = function () {
+          localStorage.setItem('league_id',JSON.stringify(this.id));
+          var dashboardHTML = './dashboard.html';
+          location.assign(dashboardHTML);
+        }
         $('#main-card').append(leagueEl)
       }
     })
